c9870d0f99eb7a9427b19032b9f57e1a06211c77
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-03-08 00:00:41 +0530

    output added

--- functions/default_parameter.js
+++ functions/default_parameter.js
@@ -42,3 +42,9 @@ function makeShoppingList(item1 = 'milk', item2='bread', item3='eggs'){
   console.log(`Remember to buy ${item3}`);
 }
 makeShoppingList();
+
+
+// Output
+Remember to buy milk
+Remember to buy bread
+Remember to buy eggs
